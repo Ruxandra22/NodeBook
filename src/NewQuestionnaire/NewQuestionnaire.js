9a385d7629146b3dcc6d16538e73e7f5fc2feeb6
Ruxandra22 <ruxandra22.osman@gmail.com>
2019-05-02 14:19:33 +0200

    send button redesigned

--- src/NewQuestionnaire/NewQuestionnaire.js
+++ src/NewQuestionnaire/NewQuestionnaire.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SaveButton from '../components/SaveButtonComponent';
 import CancelButton from '../components/CancelButtonComponent';
+import SendButton from '../components/SendButtonComponent';
 import Row from "react-bootstrap/Row";
 import "./NewQuestionnaire.css";
 import Link from "react-router-dom/es/Link";
@@ -13,12 +14,13 @@ class NewQuestionnaire extends Component {
     render() {
         let SaveBtn = SaveButton();
         let CancelBtn = CancelButton();
+        let SendBtn = SendButton();
         return (
             <div className="NewQuestionnaire container-fluid">
                 <div className="row new_questionnaire">
                     <h2 className="col-md-11 questionnaire_title">NEW QUESTIONNAIRE</h2>
                     <Popup
-                        trigger={<Button>Send</Button>}
+                        trigger={<a><div className="send_button">{SendBtn}</div></a>}
                         modal
                         closeOnDocumentClick>
                             <span>
