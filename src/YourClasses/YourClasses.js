3013b15ff983b321cee3e41642246311d27b3845
Victoria Hedenmalm <viched@kth.se>
2019-05-02 13:29:49 +0200

    Add page for class creation

--- src/YourClasses/YourClasses.js
+++ src/YourClasses/YourClasses.js
@@ -2,19 +2,9 @@ import React, { Component } from "react";
 import './YourClasses.css';
 import Link from "react-router-dom/es/Link";
 import Row from "react-bootstrap/Row";
-
-
-import Icon from '@material-ui/core/Icon';
-
-//import SaveButtonComponent from "./components/SaveButtonComponent";
-
-// import SaveButton from '../components/SaveButtonComponent';
-// import CancelButton from '../components/CancelButtonComponent';
 import CrossComponent from "../components/CrossComponent";
-
 import modelInstance from "../data/DataModel";
 import { CardGroup, Container } from "mdbreact";
-
 
 //these need to be deleted and replaced with an API call to class images
 //in DataModel.js
@@ -23,8 +13,6 @@ import classImage9B from "../images/9B.jpg"
 import classImage8A from "../images/8A.jpg"
 import classImage7C from "../images/7C.jpg"
 import classImage10B from "../images/10B.jpg"
-
-
 
 
 class ClassImage extends React.Component {
@@ -63,15 +51,12 @@ class YourClasses extends Component {
 
     componentDidMount(){
 
-     
             modelInstance.getClassNamesTest(1).then(result => {
-              
                 this.setState( {
                     status: 'SUCCESS',
                     teachersClasses: result.classes
                 })
                 console.log("result testing API", this.state.teachersClasses)
-             
             }).catch(()=>{
                 this.setState( {
                     status: 'ERROR'
@@ -87,12 +72,11 @@ class YourClasses extends Component {
         let classImages = [classImage10A , classImage9B,classImage8A ,classImage7C, classImage10B ];
 
         let i = 0;
-        let studentClassNames = this.state.teachersClasses.map((studentClass) =>
+        let studentClassNames = modelInstance.getClassNames().map((studentClass) =>
             <div id="studentClass"  key={studentClass} className="card">
-                <Link to={{pathname: '/ClassOverview/'+ studentClass.name}}>
-                    <ClassName title={"Class "+ studentClass.name} />
-                    <ClassImage image={classImages[i++]}/>
-
+                <Link to={{pathname: '/ClassOverview/'+ studentClass}}>
+                        <ClassName title={"Class "+ studentClass} />
+                        <ClassImage image={classImages[i++]}/>
                 </Link>
             </div>
         )
@@ -119,8 +103,6 @@ class YourClasses extends Component {
                     </Row>
                 </Container>
             </div>
-
-
         );
     }
 }
